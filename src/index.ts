1ad9b012bc4aad4d449f1b30c6477dd607ec4664
euglena1215 <teppest1215@gmail.com>
2023-01-15 07:55:18 +0900

    Set up RubyVM to run in the browser.
    When rubocop is required, LoadError occurs and needs to be fixed.
    
    ref. https://github.com/ruby/ruby.wasm/tree/main/packages/npm-packages/ruby-head-wasm-wasi#quick-start-for-browser

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hey!");
+import { DefaultRubyVM } from "ruby-head-wasm-wasi/dist/browser.umd";
+
+async function init() {
+  console.log("Hey!");
+  const res = await fetch("/rubocop-ast.wasm");
+
+  if (!res.ok || res.body === null) {
+    throw new Error("wasmファイルの取得に失敗しました");
+  }
+
+  const buffer = await res.arrayBuffer();
+  const module = await WebAssembly.compile(buffer);
+  const { vm } = await DefaultRubyVM(module);
+
+  const output = vm.eval(`
+    "I'm working in RubyVM!"
+    # require 'rubocop'
+    # "rubocop version is #{RuboCop::Version.version}!"
+  `);
+
+  console.log(output.toString());
+};
+
+init();
